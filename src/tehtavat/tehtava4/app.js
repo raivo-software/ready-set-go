70877339e856903cf26e7a19e333b92fe82c5355
Teemu Salminen <teemujsa@gmail.com>
2019-08-27 03:22:43 +0300

    Add fourth assignment

---
+++ src/tehtavat/tehtava4/app.js
@@ -0,0 +1,12 @@
+/*
+  Kirjoita verkkosivujen kentille toiminnallisuuksia, siten että ne muutavat sivujen taustaväriä,
+  fonttikokoa sekä otsikkoa, kun käyttäjä kirjoittaa kenttiin syötteen.
+  Arvot tulee tallentaa localStorageen, ja kun käyttäjä palaa sivuille uudestaan, tyylit asetetaan sivuille automaattisesti.
+
+  VINKIT:
+  - Saat haettua index.html:ään määritellyn elementin mm. funktiolla: document.getElementById('id');
+  - Saat määriteltyä sivun taustavärin mm. asettamalla esim.: document.body.style.backgroundColor = 'blue';
+  - Saat määriteltyä sivun fonttikoon mm. asettamalla esim.: document.body.style.fontSize = 12px;
+  - Saat määriteltyä tekstin jollekin elementille mm. asettamalla: element.innerText = 'Teksti'
+  - Saat määriteltyä tekstinsyötölle callback-kutsun mm. asettamalla: inputField.oninput = () => console.log('Tee jottain');
+*/
